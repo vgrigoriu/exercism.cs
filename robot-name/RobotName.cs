8050e19d0d7545d5ea07a27fd1e1ccc7e6907a39
Victor Grigoriu <grigoriu@adobe.com>
2019-05-22 16:55:46 -0400

    RobotName solution

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,17 +1,45 @@
 using System;
+using System.Collections.Generic;
 
-public class Robot
+public class Robot : IDisposable
 {
-    public string Name
+    private static ISet<string> names = new HashSet<string>();
+
+    private static Random random = new Random();
+
+    private string name;
+
+    public Robot()
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        name = Generate();
     }
+
+    public string Name => name;
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        names.Remove(name);
+        name = Generate();
+    }
+
+    private static string Generate()
+    {
+        string candidate;
+        do
+        {
+            candidate = $"{Letter()}{Letter()}{Digit()}{Digit()}{Digit()}";
+        }
+        while(!names.Add(candidate));
+
+        return candidate;
+    }
+
+    private static char Letter() => (char)(random.Next(26) + 'A');
+
+    private static char Digit() => (char)(random.Next(10) + '0');
+
+    public void Dispose()
+    {
+        names.Remove(name);
     }
 }
