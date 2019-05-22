5a7afb5d48ff274104e154e4563135b2b0e38655
Victor Grigoriu <grigoriu@adobe.com>
2019-05-22 16:32:00 -0400

    Hello World

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class ResistorColor
+{
+	private static readonly string[] colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
+	public static int ColorCode(string color)
+	{
+		return Array.IndexOf(colors, color);
+	}
+
+	public static string[] Colors()
+	{
+		return colors;
+	}
+}
