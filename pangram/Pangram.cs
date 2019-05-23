4e3e738c4dfaebf405a027f0a73d0f4854ddcef7
Victor Grigoriu <grigoriu@adobe.com>
2019-05-23 17:07:46 -0400

    Pangram solution

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 public static class Pangram
 {
-    public static bool IsPangram(string input)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool IsPangram(string input) => input
+            .Where(char.IsLetter)
+            .Select(char.ToLowerInvariant)
+            .Distinct()
+            .Count() == 26;
 }
