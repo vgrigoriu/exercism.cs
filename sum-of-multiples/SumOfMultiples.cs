95a88b5b1929b96e6ef7c2c2dae5e39a4987b340
Victor Grigoriu <grigoriu@adobe.com>
2019-05-23 16:46:24 -0400

    Sum of mutiples solution

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Enumerable.Range(1, max - 1).Where(IsDivisibleByAny).Sum();
+
+        bool IsDivisibleByAny(int n) => multiples.Any(d => d != 0 && n % d == 0);
     }
 }
