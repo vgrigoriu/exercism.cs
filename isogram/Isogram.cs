278654f0c4ec4a018902c8d75632b8871bf6ddbc
Victor Grigoriu <grigoriu@adobe.com>
2019-05-23 17:34:28 -0400

    Isogram

---
+++ isogram/Isogram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var letters = new HashSet<char>();
+        foreach (var ch in word)
+        {
+            if (char.IsLetter(ch) && !letters.Add(char.ToLowerInvariant(ch)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
