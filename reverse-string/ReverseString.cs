5415d48b1923e9a87f023d6007411e053d4cacd2
Victor Grigoriu <grigoriu@adobe.com>
2019-05-23 15:41:25 -0400

    Reverse string solution

--- reverse-string/ReverseString.cs
+++ reverse-string/ReverseString.cs
@@ -4,6 +4,13 @@ public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var chars = input.ToCharArray();
+        var len = chars.Length;
+        for (var i = 0; i < len / 2; i++)
+        {
+            (chars[i], chars[len - 1 - i]) = (chars[len - 1 - i], chars[i]);
+        }
+
+        return new String(chars);
     }
 }
