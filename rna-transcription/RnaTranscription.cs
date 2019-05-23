808e50c256d3f725b8764445d54d9f6e51afe4d6
Victor Grigoriu <grigoriu@adobe.com>
2019-05-23 15:56:45 -0400

    RNA Transciption solution

--- rna-transcription/RnaTranscription.cs
+++ rna-transcription/RnaTranscription.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class RnaTranscription
 {
     public static string ToRna(string nucleotide)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new String(nucleotide.Select(c => complement[c]).ToArray());
     }
+
+    private static readonly IDictionary<char, char> complement = new Dictionary<char, char>
+    {
+        {'C', 'G'},
+        {'G', 'C'},
+        {'T', 'A'},
+        {'A', 'U'}
+    };
 }
