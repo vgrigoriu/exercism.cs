2413149f5d91ea41161293e91cd95b61b02ad9aa
Victor Grigoriu <grigoriu@adobe.com>
2019-05-24 10:12:25 -0400

    Be explicit about color -> value correspondence

--- resistor-color/ResistorColor.cs
+++ resistor-color/ResistorColor.cs
@@ -1,16 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class ResistorColor
 {
-	private static readonly string[] colors = new[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+	private static readonly Dictionary<string, int> colors = new Dictionary<string, int>()
+	{
+		{"black", 0},
+		{"brown", 1},
+		{"red", 2},
+		{"orange", 3},
+		{"yellow", 4},
+		{"green", 5},
+		{"blue", 6},
+		{"violet", 7},
+		{"grey", 8},
+		{"white", 9},
+	};
 
 	public static int ColorCode(string color)
 	{
-		return Array.IndexOf(colors, color);
+		return colors[color];
 	}
 
 	public static string[] Colors()
 	{
-		return colors;
+		return colors
+			.OrderBy(kv => kv.Value)
+			.Select(kv => kv.Key)
+			.ToArray();
 	}
 }
