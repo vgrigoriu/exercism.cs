0c97ec888aad8355fde9673aa777f92258941fd0
Victor Grigoriu <grigoriu@adobe.com>
2019-05-26 19:43:05 +0300

    Leap solution

--- leap/Leap.cs
+++ leap/Leap.cs
@@ -2,8 +2,6 @@ using System;
 
 public static class Leap
 {
-    public static bool IsLeapYear(int year)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public static bool IsLeapYear(int year) =>
+        year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
