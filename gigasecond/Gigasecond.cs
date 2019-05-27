e7405e7b792547903639ca45e1675e388344c7f1
Victor Grigoriu <grigoriu@adobe.com>
2019-05-27 11:04:51 +0300

    Gigasecond solution

--- gigasecond/Gigasecond.cs
+++ gigasecond/Gigasecond.cs
@@ -2,8 +2,7 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    private const double Giga = 1e9;
+
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(Giga);
 }
