38965aac13a5954524b7166225e1fbb29cd2413b
Victor Grigoriu <grigoriu@adobe.com>
2019-05-28 19:07:16 +0300

    Space Age solution

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -1,48 +1,65 @@
 using System;
+using System.Collections.Generic;
 
 public class SpaceAge
 {
+    private const double secondsInEarthYear = 365.25 * 24 * 60 * 60;
+
+    private static readonly Dictionary<string, double> factor = new Dictionary<string, double>
+    {
+        {"Mercury", 0.2408467},
+        {"Venus", 0.61519726},
+        {"Mars", 1.8808158},
+        {"Jupiter", 11.862615},
+        {"Saturn", 29.447498},
+        {"Uranus", 84.016846},
+        {"Neptune", 164.79132},
+    };
+
+    private readonly int seconds;
+
     public SpaceAge(int seconds)
     {
+        this.seconds = seconds;
     }
 
     public double OnEarth()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return seconds / secondsInEarthYear;
     }
 
     public double OnMercury()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Mercury"];
     }
 
     public double OnVenus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Venus"];
     }
 
     public double OnMars()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Mars"];
     }
 
     public double OnJupiter()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Jupiter"];
     }
 
     public double OnSaturn()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Saturn"];
     }
 
     public double OnUranus()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Uranus"];
     }
 
     public double OnNeptune()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return OnEarth() / factor["Neptune"];
     }
 }
