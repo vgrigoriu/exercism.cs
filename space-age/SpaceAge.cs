b274cc50b0916bf02eda6b93f13e6562e3d7f7b2
Victor Grigoriu <grigoriu@adobe.com>
2019-05-29 16:50:26 +0300

    Space Age: use enum

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -5,16 +5,28 @@ public class SpaceAge
 {
     private const double secondsInEarthYear = 365.25 * 24 * 60 * 60;
 
-    private static readonly Dictionary<string, double> factor = new Dictionary<string, double>
+    private static readonly Dictionary<Planet, double> factor = new Dictionary<Planet, double>
     {
-        {"Mercury", 0.2408467},
-        {"Venus", 0.61519726},
-        {"Mars", 1.8808158},
-        {"Jupiter", 11.862615},
-        {"Saturn", 29.447498},
-        {"Uranus", 84.016846},
-        {"Neptune", 164.79132},
+        {Planet.Mercury, 0.2408467},
+        {Planet.Venus, 0.61519726},
+        {Planet.Mars, 1.8808158},
+        {Planet.Jupiter, 11.862615},
+        {Planet.Saturn, 29.447498},
+        {Planet.Uranus, 84.016846},
+        {Planet.Neptune, 164.79132},
     };
+
+    private enum Planet
+    {
+        Earth,
+        Mercury,
+        Venus,
+        Mars,
+        Jupiter,
+        Saturn,
+        Uranus,
+        Neptune
+    }
 
     private readonly int seconds;
 
@@ -30,36 +42,36 @@ public class SpaceAge
 
     public double OnMercury()
     {
-        return OnEarth() / factor["Mercury"];
+        return OnEarth() / factor[Planet.Mercury];
     }
 
     public double OnVenus()
     {
-        return OnEarth() / factor["Venus"];
+        return OnEarth() / factor[Planet.Venus];
     }
 
     public double OnMars()
     {
-        return OnEarth() / factor["Mars"];
+        return OnEarth() / factor[Planet.Mars];
     }
 
     public double OnJupiter()
     {
-        return OnEarth() / factor["Jupiter"];
+        return OnEarth() / factor[Planet.Jupiter];
     }
 
     public double OnSaturn()
     {
-        return OnEarth() / factor["Saturn"];
+        return OnEarth() / factor[Planet.Saturn];
     }
 
     public double OnUranus()
     {
-        return OnEarth() / factor["Uranus"];
+        return OnEarth() / factor[Planet.Uranus];
     }
 
     public double OnNeptune()
     {
-        return OnEarth() / factor["Neptune"];
+        return OnEarth() / factor[Planet.Neptune];
     }
 }
