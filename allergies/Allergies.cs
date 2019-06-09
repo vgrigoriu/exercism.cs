dd5b0f54c4bf09e162825ceddf8658cc3d7258af
Victor Grigoriu <vgrigoriu@gmail.com>
2019-06-09 19:09:01 +0300

    Allergies solution

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -1,30 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
+[Flags]
 public enum Allergen
 {
-    Eggs,
-    Peanuts,
-    Shellfish,
-    Strawberries,
-    Tomatoes,
-    Chocolate,
-    Pollen,
-    Cats
+    Eggs = 1,
+    Peanuts = 2,
+    Shellfish = 4,
+    Strawberries = 8,
+    Tomatoes = 16,
+    Chocolate = 32,
+    Pollen = 64,
+    Cats = 128
 }
 
 public class Allergies
 {
-    public Allergies(int mask)
-    {
-    }
+    private static readonly List<Allergen> allAllergens = Enum.GetValues(typeof(Allergen)).OfType<Allergen>().ToList();
+    private readonly Allergen mask;
 
-    public bool IsAllergicTo(Allergen allergen)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public Allergies(int mask) : this((Allergen)mask) { }
+
+    public Allergies(Allergen mask) => this.mask = mask;
+
+    public bool IsAllergicTo(Allergen allergen) => mask.HasFlag(allergen);
 
     public Allergen[] List()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return allAllergens
+            .Where(IsAllergicTo)
+            .ToArray();
     }
 }
