26d0ae6fcfd27db4787d9d2e3ae066a157730191
Victor Grigoriu <grigoriu@adobe.com>
2019-06-16 10:06:10 +0300

    Rot cipher solution

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,30 @@
-using System;
+using System.Text;
 
 public static class RotationalCipher
 {
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        StringBuilder sb = new StringBuilder();
+        foreach (var ch in text)
+        {
+            sb.Append(Rotate(ch, shiftKey));
+        }
+
+        return sb.ToString();
+    }
+
+    private static char Rotate(char ch, int shiftKey)
+    {
+        if ('a' <= ch && ch <= 'z')
+        {
+            return (char)('a' + (ch - 'a' + shiftKey) % 26);
+        }
+
+        if ('A' <= ch && ch <= 'Z')
+        {
+            return (char)('A' + (ch - 'A' + shiftKey) % 26);
+        }
+
+        return ch;
     }
 }
