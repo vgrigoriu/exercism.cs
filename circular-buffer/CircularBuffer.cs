641f89b666889a656d7434aa0fafe74497b529e9
Victor Grigoriu <vgrigoriu@gmail.com>
2019-06-17 19:49:40 +0300

    first test

--- circular-buffer/CircularBuffer.cs
+++ circular-buffer/CircularBuffer.cs
@@ -2,14 +2,26 @@
 
 public class CircularBuffer<T>
 {
+    private readonly T[] buffer;
+
+    // the index of the first element in the buffer
+    private int begin = 0;
+
+    private int length = 0;
+
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        buffer = new T[capacity];
     }
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (length == 0)
+        {
+            throw new InvalidOperationException("Can't read from empty buffer");
+        }
+
+        return default;
     }
 
     public void Write(T value)
