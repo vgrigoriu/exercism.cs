0b36ee06fee9db3738a9adc3dae91a82483de723
Victor Grigoriu <grigoriu@adobe.com>
2019-07-12 18:00:28 +0300

    Comments

--- spiral-matrix/SpiralMatrix.cs
+++ spiral-matrix/SpiralMatrix.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-public class SpiralMatrix
+﻿public class SpiralMatrix
 {
     public static int[,] GetMatrix(int size)
     {
@@ -23,23 +21,28 @@ public class SpiralMatrix
         var lastRow = firstRow + squareLength - 1;
         var value = startValue;
 
+        // top side
         for (var col = firstCol; col < lastCol; col += 1) {
             matrix[firstRow, col] = value;
             value += 1;
         }
+        // right side
         for (var row = firstRow; row < lastRow; row += 1) {
             matrix[row, lastCol] = value;
             value += 1;
         }
+        // bottom side
         for (var col = lastCol; col > firstCol; col -= 1) {
             matrix[lastRow, col] = value;
             value += 1;
         }
+        // left side
         for (var row = lastRow; row > firstRow; row -= 1) {
             matrix[row, firstCol] = value;
             value += 1;
         }
 
+        // recurse to the inner square
         Fill(matrix, squareLength - 2, firstRow + 1, firstCol + 1, value);
     }
 }
