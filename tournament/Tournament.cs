9acc0cdfacf49ba33b16cfc98353e61e1177554f
Victor Grigoriu <grigoriu@adobe.com>
2019-07-31 10:17:24 +0300

    add Match class

--- tournament/Tournament.cs
+++ tournament/Tournament.cs
@@ -14,14 +14,64 @@ public static class Tournament
             writer.Write("Team                           | MP |  W |  D |  L |  P");
             if (input != null)
             {
-                var parts = input.Split(";");
-                var team1 = parts[0];
-                var team2 = parts[1];
+                var match = Match.Parse(input);
                 writer.WriteLine();
-                writer.Write(team1 + "             |  1 |  1 |  0 |  0 |  3");
+                writer.Write(match.Team1 + "             |  1 |  1 |  0 |  0 |  3");
                 writer.WriteLine();
-                writer.Write(team2 + "             |  1 |  0 |  0 |  1 |  0");
+                writer.Write(match.Team2 + "             |  1 |  0 |  0 |  1 |  0");
             }
         }
     }
+}
+
+public enum Result
+{
+    Win,
+    Loss
+}
+
+public class Match
+{
+    public static Match Parse(string line)
+    {
+        Result ParseResult(string result)
+        {
+            switch (result)
+            {
+                case "win":
+                    return Result.Win;
+                case "loss":
+                    return Result.Loss;
+                default:
+                    throw new ArgumentException($"Invalid result: {result}");
+            }
+        }
+        var parts = line.Split(";");
+        if (parts.Length != 3)
+        {
+            throw new ArgumentException($"Not a valid input line: {line}");
+        }
+        return new Match(parts[0], parts[1], ParseResult(parts[2]));
+    }
+
+    private Match(string team1, string team2, Result result)
+    {
+        if (team1 is null)
+        {
+            throw new ArgumentNullException(nameof(team1));
+        }
+
+        if (team2 is null)
+        {
+            throw new ArgumentNullException(nameof(team2));
+        }
+
+        Team1 = team1;
+        Team2 = team2;
+        Result = result;
+    }
+
+    public string Team1 { get; }
+    public string Team2 { get; }
+    public Result Result { get; }
 }
